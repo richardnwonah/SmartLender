58c378f03181cdfc5e19f9b43de660aaf17ed502
Richard <nwonahr@gmail.com>
2024-08-12 11:55:24 +0100

    Backup, Interface implementation

---
+++ SmartLeader.App/SmartLeader.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using SmartLeader.Api.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SmartLeader.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<CreditScore> CreditScores { get; set; }
+        public DbSet<LoanQualification> LoanQualifications { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ SmartLeader.App/SmartLeader.Api/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartLeader.Api.Data;
+using SmartLeader.Api.Models;
+
+namespace SmartLeader.Api.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.Include(u => u.CreditScore).ToListAsync();  
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.Include(u => u.CreditScore)
+                .FirstAsync(u => u.Id == id);
+        }
+
+        public async Task AddAsync(User entity)
+        {
+            await _context.Users.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
